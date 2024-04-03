976603a20631b99e7a8ff81efed1ab48e70f951d
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-04-03 16:46:12 +0300

    fix: the application has been brought to a working version

---
+++ app/routes/errors.py
@@ -0,0 +1,8 @@
+from app.adapters.content_collector import content_collector_to_dict
+from flask import render_template
+
+# Обработка ошибки 404 - страница не найдена
+def page_not_found(e, form):
+    filling = content_collector_to_dict(page='page_not_found', contacts='contacts')
+    
+    return render_template('detailed_page.html', filling=filling, form=form), 404
---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from app.routes.form_bid import ContactForm
+import app.routes.pages as pages
+
+
+app = Flask(__name__, template_folder='app/templates')
+form = ContactForm()
+app.register_blueprint(pages.bp)
+
+
+if __name__ == '__main__':
+  # debug true задаем специально для разработки 
+  # (в данном случае при обновление/изменение кода приложение 
+  # автоматически само обновит данные на сайте)
+  app.run(debug=True) 
+
