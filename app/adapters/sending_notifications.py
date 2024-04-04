dd0370e4427d1eb81fefc97ffb2a7daf1d26e97e
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-04-04 16:22:18 +0300

    refactor: methods for sending messages have been replaced with the SenderOfMessages class
    Methods for sending messages have been replaced by the SenderOfMessages class. The email.ini settings file contains the paths to the templates.

--- app/adapters/sending_notifications.py
+++ app/adapters/sending_notifications.py
@@ -4,85 +4,100 @@ from configparser import ConfigParser
 import jinja2
 
 
-setting_path = "setting\email.ini"
-
-
-def file_availability_check(file_path: str) -> bool:
+class SenderOfMessages():
     '''
-    Проверка наличия файла ini
+    Отправитель сообщений
     '''
+    
+    def __init__(self, setting_path:str) -> bool:
 
-    base_path = os.path.dirname(os.path.abspath(__file__))
-    config_path = os.path.join(base_path, file_path)
-
-    return os.path.exists(config_path)
+        if self.file_availability_check(setting_path):
+            self.sender_settings(setting_path)
 
 
-def send_bid(massage:str) -> None:
-    '''
-    Отправка сообщения на почту
-    '''
+    def sender_settings(self, setting_path:str) -> None:
+        '''
+        Чтение настроек из файла ini
+        '''
 
-    if file_availability_check(setting_path):
         config = ConfigParser()
-        config.read('setting\email.ini')
+        config.read(setting_path)
+            # Настройки
+        self.mime = config.get("setting", "mime")
+        self.charset = config.get("setting", "charset")
+        self.server = config.get("setting", "server")
+        self.port = config.get("setting", "port")
 
-        # Настройки
-        mime = config.get("setting", "mime")
-        charset = config.get("setting", "charset")
-        server = config.get("setting", "server")
-        port = config.get("setting", "port")
+        self.user = config.get("personal data", "email")
+        self.passwd = config.get("personal data", "passwd")
+        self.to = config.get("personal data", "email")
 
-        user = config.get("personal data", "email")
-        passwd = config.get("personal data", "passwd")
-        to = config.get("personal data", "email")
-        subject = config.get("setting letter", "subject")
-    else:
-        return False
+        self.subject = config.get("setting letter", "subject")
 
-    # формируем тело письма
-    body = "\r\n".join((f"From: {user}", f"To: {to}", 
-        f"Subject: {subject}", mime, charset, "", massage))
+        self.path_to_letter_without_message = config.get("setting letter", "path_to_letter_without_message")
+        self.path_to_letter_with_message = config.get("setting letter", "path_to_letter_with_message")
 
-    try:
-        # подключаемся к почтовому сервису
-        smtp = smtplib.SMTP(server, port)
+
+    def file_availability_check(self, file_path: str) -> bool:
+        '''
+        Проверка наличия файла ini
+        '''
+
+
+        return os.path.exists(file_path)
+
+
+    def setting_letter(self, message:str) -> str:
+        '''
+        Настройка содержания пиьсма
+        '''
+
+        body = "\r\n".join((f"From: {self.user}", f"To: {self.to}", 
+        f"Subject: {self.subject}", self.mime, self.charset, "", message))
+
+        return body
+    
+
+    def send_bid(self, body_message:str) -> None:
+        '''
+        Отправка сообщения на почту
+        '''
+        smtp = smtplib.SMTP(self.server, self.port)
         smtp.starttls()
         smtp.ehlo()
         # логинимся на почтовом сервере
-        smtp.login(user, passwd)
+        smtp.login(self.user, self.passwd)
         # пробуем послать письмо
-        smtp.sendmail(user, to, body.encode('utf-8'))
-    except smtplib.SMTPException as err:
-        print('Что - то пошло не так...')
-        raise err
-    finally:
+        smtp.sendmail(self.user, self.to, body_message.encode('utf-8'))
         smtp.quit()
 
 
-def render_letter(**values) -> str:
-    '''
-    Сборка письма по данным формы
-    '''
 
-    filename = "letter_sample_with_massage.txt"
-    username = values["username"]
-    number = values["number"]
-    message = values["message"]
+    def render_letter(self, username:str, phonnumber:str, message=None) -> str:
+        '''
+        Вставка данных в шаблон
+        '''
 
-    if message==None:
-        filename="letter_sample_only_name_and_number.txt"
+        filename = self.path_to_letter_with_message
+        if message==None:
+            filename=self.path_to_letter_without_message
 
-    with open(f"templates\{filename}", 'r', encoding='utf-8') as template_file:
-        template_file_content = template_file.read()
-    environment = jinja2.Environment()
-    template = environment.from_string(template_file_content)
-    letter = template.render(username=username,
-                             number=number,
-                             message=message)
-    
-    return letter
+        with open(filename, 'r', encoding='utf-8') as template_file:
+            template_file_content = template_file.read()
+        environment = jinja2.Environment()
+        template = environment.from_string(template_file_content)
+        letter = template.render(username=username,
+                                number=phonnumber,
+                                message=message)
+        
+        return letter
 
 
-def sending_notifications (**values) -> None:
-    send_bid(render_letter(**values))
+    def sending_notifications (self, username:str, phonnumber:str, message=None) -> None:
+        '''
+        Главный метод-менеджер, принимающий данные и отправляющий их на почту
+        '''
+        text_letter = self.render_letter(username=username, phonnumber=phonnumber, message=message)
+        self.send_bid(self.setting_letter(text_letter))
+
+
