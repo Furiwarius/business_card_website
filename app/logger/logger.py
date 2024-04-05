46eabd0626e760d6c4356bb162d41659b08c9780
Vasiliy <vasiliy.trunov.00@mail.ru>
2024-04-05 16:35:51 +0300

    feat: added logging module, log settings

---
+++ app/logger/logger.py
@@ -0,0 +1,10 @@
+import logging
+import logging.config
+
+log_setting = 'app\logger\log_setting\log.conf'
+
+logging.config.fileConfig(fname=log_setting)
+
+logger = logging.getLogger('dev')
+
+logger.info('info message')
